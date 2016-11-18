047daa994ac750499c6b66261d8a058a978c4253
mengyuan411 <574266647@qq.com>
2016-11-18 16:07:47 +0800

    fix kaixin_au bug

--- script/kaixin_au.sh
+++ script/kaixin_au.sh
@@ -1,16 +1,19 @@
+rm $2
+
 mac=`ifconfig wlan0 | grep HWaddr | awk '{print $5}'|sed 's/://g'`
 echo $mac
-A1=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel active time" | grep -o "[0-9]*"`
-B1=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel busy time"   | grep -o "[0-9]*"`
-R1=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel receive time"  | grep -o "[0-9]*"`
-T1=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel transmit time" | grep -o "[0-9]*"`
+iw $1 survey dump > /tmp/aulog
+A1=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel active time" | grep -o "[0-9]*"`
+B1=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel busy time"   | grep -o "[0-9]*"`
+R1=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel receive time"  | grep -o "[0-9]*"`
+T1=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel transmit time" | grep -o "[0-9]*"`
 
 sleep 5
-
-A2=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel active time" | grep -o "[0-9]*"`
-B2=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel busy time"   | grep -o "[0-9]*"`
-R2=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel receive time"  | grep -o "[0-9]*"`
-T2=`iw $1 survey dump | grep -F "[in use]" -A5 | grep -e "channel transmit time" | grep -o "[0-9]*"`
+iw $1 survey dump > /tmp/aulog
+A2=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel active time" | grep -o "[0-9]*"`
+B2=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel busy time"   | grep -o "[0-9]*"`
+R2=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel receive time"  | grep -o "[0-9]*"`
+T2=`cat /tmp/aulog | grep -F "[in use]" -A5 | grep -e "channel transmit time" | grep -o "[0-9]*"`
 
 ALL=`expr $A2 - $A1`
 
@@ -28,4 +31,4 @@ IU=`expr $IF "*" 100 "/" $ALL`
 Tt=`date "+%D %T"`
 Ts=`date "+%s"`
 
-echo "$Tt,$IU,$LU,$Ts,$AP,$IF,$LOAD,$ALL" >> /tmp/wifiunion-uploads/$mac/au_$1.txt
+echo "$Tt,$IU,$LU,$Ts,$AP,$IF,$LOAD,$ALL" >> $2
